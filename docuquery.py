fc09e445cb03b9fe1eeaef1957c9126c6a718af5
Viktor Tisza <vtisza@gmail.com>
2023-09-30 22:43:19 +0200

    replace docx reader package

--- docuquery.py
+++ docuquery.py
@@ -1,9 +1,10 @@
 import streamlit as st
-from docx import Document
 
 from langchain.prompts.prompt import PromptTemplate
 from langchain.llms import OpenAI
 from langchain.chains import LLMChain
+
+import docx2txt
 
 
 class WordSuccessChecker:
@@ -11,6 +12,17 @@ class WordSuccessChecker:
         self.uploaded_file = None
         self.doc_text = None
         self.input_text = None
+        self._validation_prompt = """
+        You are a highly trained assistant who reads through input documents and answer questions about them. Table elements are delimeted by | sign.
+        This is the input document: {doc_text}
+
+        You get the following question: {input_text}
+
+        Provide a accurate and informative answer only based on the document. If question cannot be answered based on the input document do not make up an answer just tell answer is not in the text. """
+        self.VALIDATION_PROMPT = PromptTemplate(
+            input_variables=["doc_text", "input_text"],
+            template=self._validation_prompt
+        )
 
     def query_doc(self):
         if len(self.doc_text.split()) < 5000:
@@ -20,43 +32,17 @@ class WordSuccessChecker:
 
 
     def query_doc_short(self):
-        _validation_prompt = """
-        You are a highly trained assistant who reads through input documents and answer questions about them.
-        This is the input document: {doc_text}
-
-        You get the following question: {input_text}
-
-        Provide a accurate and informative answer only based on the document. If question cannot be answered based on the input document do not make up an answer just tell answer is not in the text. """
-
-        VALIDATION_PROMPT = PromptTemplate(
-            input_variables=["doc_text", "input_text"],
-            template=_validation_prompt
-        )
-
         llm_chain = LLMChain(
             llm=OpenAI(temperature=0, max_tokens=-1, model_name="gpt-3.5-turbo-16k"),
-            prompt=VALIDATION_PROMPT
+            prompt=self.VALIDATION_PROMPT
         )
 
         return llm_chain({"doc_text": self.doc_text, "input_text": self.input_text})["text"]
     
     def query_doc_long(self):
-        _validation_prompt = """
-        You are a highly trained assistant who reads through input documents and answer questions about them.
-        This is the input document: {doc_text}
-
-        You get the following question: {input_text}
-
-        Provide a accurate and informative answer only based on the document. If question cannot be answered based on the input document do not make up an answer just tell answer is not in the text. """
-
-        VALIDATION_PROMPT = PromptTemplate(
-            input_variables=["doc_text", "input_text"],
-            template=_validation_prompt
-        )
-
         llm_chain = LLMChain(
             llm=OpenAI(temperature=0, max_tokens=-1, model_name="gpt-3.5-turbo-16k"),
-            prompt=VALIDATION_PROMPT
+            prompt=self.VALIDATION_PROMPT
         )
 
         return llm_chain({"doc_text": self.doc_text[:14000], "input_text": self.input_text})["text"]
@@ -70,8 +56,7 @@ class WordSuccessChecker:
 
         if self.uploaded_file is not None:
             # Read the uploaded Word document
-            doc = Document(self.uploaded_file)
-            self.doc_text = "\n".join([para.text for para in doc.paragraphs])
+            self.doc_text = docx2txt.process(self.uploaded_file)
 
             # User input text
             self.input_text = st.text_input("Enter a question to query the uploaded document:")
@@ -79,7 +64,7 @@ class WordSuccessChecker:
             if st.button("Query"):
                 if self.input_text:
                     result = self.query_doc()
-                    st.write(f"Result: {result}")
+                    st.write(f"Answer: {result}")
                 else:
                     st.write("Please enter a question.")
 
